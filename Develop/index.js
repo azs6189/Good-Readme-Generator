3bf8b430aaf62bcbe1df501a7ab57fceb3da3d4b
Allie Soliz <solizale@gmail.com>
2021-10-11 19:54:48 -0400

    updated index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,45 +1,94 @@
-// TODO: Include packages needed for this application
-
-// Variables
-const inquirer = require("inquirer");
-const fs = require("fs");
-const generateMarkdown = require(".utils/generateMarkdown.js");
-const fileName = "README.md";
-// TODO: Create an array of questions for user input
-const questions = [
-	{
-		type: "input",
-		name: "title",
-		message: "What is the title of your project?",
-	},
-{
-    type: "input",
-    name: "description"
-    message: "Please include a description of your project:"
-},
-{
-    type: "list",
-    name: "license",
-    message: "Please select a license for your project.",
-    choices: ['MIT',"ISC","Apache"]
-},
-{
-    type: "input",
-    name: "GitHub"
-    message: "What is your GitHub username?"
-},
-{
-    type: "input",
-    name: "email",
-    message: "What is your email address?"
+// Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+	let url = ``;
+	switch (license) {
+		case "MIT":
+			url = `![licenseShield](https://img.shields.io/badge/license-MIT-yellow)`;
+			break;
+		case "GPL":
+			url = `![licenseShield](https://img.shields.io/badge/license-GPL-blue)`;
+			break;
+		case "Apache 2.0":
+			url = `![licenseShield](https://img.shields.io/badge/license-apache%202.0-blue)`;
+			break;
+	}
+	return url;
 }
-];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+	let url = ``;
+	if (license === "None") {
+		return url;
+	}
+	url = "/LICENSE";
+	return url;
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function that returns the license section of README and Table of Contents
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+	if (license === "None") {
+		return ``;
+	}
+	return `## License
+    This project is registered under the [${license} License](${renderLicenseLink(
+		license
+	)})`;
+}
 
-// Function call to initialize app
-init();
+function renderLicenseTableofContents(license) {
+	if (license === "None") {
+		return ``;
+	}
+	return `* [License](#license)`;
+}
+
+// Create a list when input items are separated by a comma
+function renderList(list) {
+	const listArr = [];
+	list = list.split(",");
+	list.forEach((listItem) => {
+		listArr.push(`* ${listItem}`);
+	});
+	return listArr.join("\n  ");
+}
+
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+	return `# ${data.title}
+    ${renderLicenseBadge(data.license)}
+    
+    ## Table of Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseTableofContents(data.license)}
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## Description
+    ${data.description}
+    ## Installation
+    ${data.installation}
+    ## Usage
+    ${data.usage}
+    ${renderLicenseSection(data.license)}
+    ## Contributing
+    ${renderList(data.contributors)}
+    
+    ## Tests
+    ${renderList(data.tests)}
+    ## Questions
+    * __GitHub:__ [github.com/${data.gitHub}](https://github.com/${data.gitHub})
+    * __Email:__ [${data.email}](mailto:${data.email})
+     
+    
+    _This README was created using the [README Generator](https://github.com/azs6189/README-Generator)_
+  `;
+}
+
+module.exports = generateMarkdown;
